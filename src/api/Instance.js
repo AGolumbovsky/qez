f2db3e92c7ec9afd0f8ab8cf6165d5f1b8113a6c
Andriy Golumbovsky <agolumbovsky@gmail.com>
2021-11-24 13:21:25 -0500

    defaultUrl to baseUrl. now we're cookin'

--- src/api/Instance.js
+++ src/api/Instance.js
@@ -1,6 +1,5 @@
 export const request = (url, config) => {
     // const defaultUrl = "https://opentdb.com/api.php";
-    const defaultUrl = "http://localhost:3000/";
-    return fetch(`${ defaultUrl }${ url }`, config)
+    const baseUrl = "http://localhost:3000/";
+    return fetch(`${ baseUrl }${ url }`, config)
 }
-
