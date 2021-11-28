20fb77eb8e1fc96e36d8843f32dabb3172507b02
Andriy Golumbovsky <agolumbovsky@gmail.com>
2021-11-28 09:03:25 -0500

    catch up

--- src/Components/StartTestButton.js
+++ src/Components/StartTestButton.js
@@ -1,70 +1,35 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { getQuestions } from '../api/routes/getQuestions'
 import { Button } from '../componentsTemp/Button'
 
-function StartTestButton(props) {
-  
-  const questionDifficulty = useSelector(
-    (state) => state.options.question_difficulty
-  )
-  const questionType = useSelector((state) => state.options.question_type)
-  
-  const questionIndex = useSelector((state) => state.index)
+function StartTestButton (props) {
 
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const setLoading = (value) => {
-    dispatch({
-      type: 'CHANGE_LOADING',
-      loading: value,
-    })
-  }
+    const setLoading = (value) => {
+        dispatch({
+            type: 'CHANGE_LOADING',
+            loading: value,
+        })
+    }
 
-  const setQuestions = (value) => {
-    dispatch({
-      type: 'SET_QUESTIONS',
-      questions: value,
-    })
-  }
-
-  const handleQuery = async () => {
-    let someNumber = 3; // hard coding the number of questions
-    let apiUrl = `https://opentdb.com/api.php?amount=${someNumber}`
+    const handleQuestions = (value) => {
+        dispatch({
+            type: 'SET_QUESTIONS',
+            questions: value,
+        })
+    }
     
-    setLoading(true)
-
-    
-
-    if (questionDifficulty.length) {
-      apiUrl = apiUrl.concat(`&difficulty=${questionDifficulty}`)
+    const handleQuery = async () => {
+        
+        setLoading(true)
+        
+        getQuestions()
+       
     }
 
-    if (questionType.length) {
-      apiUrl = apiUrl.concat(`&type=${questionType}`)
-    }
-
-    setLoading(true)
-
-    await fetch(apiUrl)
-      .then((res) => res.json())
-      .then((response) => {
-        setQuestions(response.results)
-        setLoading(false)
-      })
-
-    if (questionIndex > 0) {
-      dispatch({
-        type: 'SET_INDEX',
-        index: 0,
-      })
-
-      dispatch({
-        type: 'SET_SCORE',
-        score: 0,
-      })
-    }
-  }
-
-  return <Button onClick={ handleQuery }>Start the Test</Button>
+    return <Button onClick={ handleQuery }>Pehali!</Button>
 }
+
 export default StartTestButton
