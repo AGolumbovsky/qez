10ebc16ceca22de2358afe1f2eb1908d9ad7c794
Andriy Golumbovsky <agolumbovsky@gmail.com>
2022-01-24 06:31:21 -0500

    don't remember

---
+++ src/Components/Modal.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+export const Modal = () => {
+
+return (
+    <div class="modal" >
+        <div class="modal-content">{ props.description }</div>    
+    </div>
+)
+
+}
--- src/Components/ResultsScreen.js
+++ src/Components/ResultsScreen.js
@@ -2,27 +2,40 @@ import { process_params } from 'express/lib/router'
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Button } from '../componentsTemp/Button'
+import { Modal } from './Modal'
 
 export const ResultsScreen = () => {
 
     const allQuestions = useSelector(state => state.questions)
     const userAnswers = useSelector(state => state.userAnswers)
+    const [state, setState] = useState(false)
+    // create another local state where you keep the correct answer
 
     const dispatch = useDispatch()
     
-    const handleGrading = (qId) => {
+    console.log("allQuestions is:", allQuestions)
+    console.log("userAnswers is:", userAnswers)
+
+    const handleOpenModal = (question) => {
         
+        setState(true)
+        // call the other local state
+
         
     }
-
-
-    return (<div> { allQuestions.map(qu => <p className={ userAnswers[qu.id] ? "" : "gr" }> 
+    
+    
+    return (<div> { allQuestions.map(qu => <p className={ 
+        userAnswers[qu.id] === qu.correctAnswerId ? "correct-answer" : "wrong-answer"}
+        onClick={ handleOpenModal }
+        > 
+        {console.log(state)}
     { qu.question }
     </p>) 
     }
+    { state && <Modal ></Modal> }
+    <Button >Back to Main</Button>
     
-    <Button >Go Back to Main</Button>
-    
-</div>)
+    </div>)
 
 }
