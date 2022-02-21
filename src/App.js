4f93a5535a786fe91095b982eac5408840027a21
Andriy Golumbovsky <agolumbovsky@gmail.com>
2022-02-21 11:42:35 -0500

    created StartPage and added a dropdown

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ import './App.css'
 import { useEffect } from 'react'
 import { getQuestions } from './api/routes/getQuestions'
 import StartTestButton from './Components/StartTestButton'
+import StartPage from './Components/StartPage'
 
 function App() {
   const questions = useSelector((state) => state.questions)
   const questionIndex = useSelector((state) => state.index)
 
 
+  // return (
+  //   <div className="App">
+  //     <div className="app-container">{ (questions.length != 0 ? <Question /> : <StartTestButton />)}</div>
+  //   </div>
+  // )
+
   return (
     <div className="App">
-      <div className="app-container">{ (questions.length != 0 ? <Question /> : <StartTestButton />)}</div>
+      <div className="app-container"> { (questions.length != 0 ? <Question /> : <StartPage />)}</div>
     </div>
   )
 }
---
+++ src/Components/StartPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import StartTestButton from './StartTestButton'
+
+function StartPage () {
+    return (
+
+        <div>
+            <select>
+                <option>uno</option>
+                <option>dos</option>
+            </select>
+            
+            <StartTestButton/>
+        </div>
+    )
+}
+
+export default StartPage
